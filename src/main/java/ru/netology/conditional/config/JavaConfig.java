6e2a341449d3bba70220b0cecd970e01114250de
“Yuri <yura.kopshev@gmail.com>
2021-09-22 16:54:44 +0400

    Initial commit

---
+++ src/main/java/ru/netology/conditional/config/JavaConfig.java
@@ -0,0 +1,27 @@
+package ru.netology.conditional.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import ru.netology.conditional.DevProfile;
+import ru.netology.conditional.ProductionProfile;
+import ru.netology.conditional.SystemProfile;
+
+@Configuration
+
+public class JavaConfig {
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
